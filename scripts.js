9e8b3ded65ef05d84ddfe65adbf4c8b9727ac1c3
EnergyDrinksRated <33663566+EnergyDrinksRated@users.noreply.github.com>
2018-01-26 15:59:41 +0000

    FIXED MOBILE SCALING

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
-function searchIngredients() {
-    var input, filter, allIngredients, li, a, i;
-    input = document.getElementByClass("searchIngredients");
-    filter = input.value.toUpperCase();
-    ul = document.getElementByClass("allIngredients");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+//table search from w3schools//
 
-        }
-    }
+function searchFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("ingredientInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ingredientsTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
 }
+
+//back to top//
 
 function backToTop() {
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
